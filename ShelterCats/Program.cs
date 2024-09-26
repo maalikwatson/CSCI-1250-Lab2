60021101c62cdeed592ddfa6214f85b2a1a70add
maalikwatson <watsmaa@outlook.com>
2024-09-26 16:43:02 -0400

    Add project files.

---
+++ ShelterCats/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("***** Welcome to Shelter Cats App *****");
+Console.WriteLine("\n");
+
+Console.WriteLine("Please enter total number of female cats: ");
+int femaleCats = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter total number of male cats: ");
+int maleCats = Convert.ToInt32(Console.ReadLine());
+
+// (value/total value)×100%
+
+
+
+Console.ReadKey();
